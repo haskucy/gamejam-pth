de409a1bfddda8f82951f7ae1c4d693647186d81
Rendya Yuschak <rendya.yuschak@ui.ac.id>
2021-10-13 11:28:17 +0700

    ADD: initialize project with template

---
+++ src/Scenes/LinkButton.gd
@@ -0,0 +1,8 @@
+extends LinkButton
+
+export(String) var scene_to_load
+
+func _on_LinkButton_pressed():
+	print(scene_to_load)
+	get_tree().change_scene(str("res://Scenes/" + scene_to_load + ".tscn"))
+	
